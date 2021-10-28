385b5c39046a079373b116131decddb39798a85f
User <User@LAPTOP-5B8U3CFR>
2021-10-28 08:24:50 +0700

    Add project files.

---
+++ model/Seller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreshFruit.model
+{
+    class Seller
+    {
+        private string name;
+        private Controller.BucketController bucket;
+        public Seller(string name, Controller.BucketController bucket)
+        {
+            this.name = name;
+            this.bucket = bucket;
+        }
+        public List<Fruit> findAll()
+        {
+            return this.bucket.findAll();
+        }
+        public void addFruit(Fruit fruit)
+        {
+            this.bucket.addFruit(fruit);
+        }
+    }
+}
